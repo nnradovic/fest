185957884a2b786c6f58f4f26eb46f6d84cc9eed
Nikola Radovic <n.radovic@betabit.nl>
2022-01-26 15:18:26 +0100

    trt

--- App.js
+++ App.js
@@ -1,24 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import codePush from "react-native-code-push";
 
-let codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_START };
-
-let App: () => Node = () => {
-  useEffect(() => {
-    codePush.sync({
-      updateDialog: true,
-      installMode: codePush.InstallMode.IMMEDIATE,
-    });
-  }, []);
+export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your a2pp!Promenjeno malo!</Text>
+      <Text>
+        Open up App.js to start working on your a2pp!Promenjeno mal3o!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -28,5 +21,3 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
-
-export default codePush(codePushOptions)(App);
